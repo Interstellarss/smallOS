02881550495a1aac8e3652fb8b2f251a8dde2dac
Puxuan Wang <wangpu@in.tum.de>
2021-03-21 22:40:52 +0100

    update for printf

--- kernel.cpp
+++ kernel.cpp
@@ -3,8 +3,37 @@
 void printf(char* str){
     //0xb800 is the address for graphic card
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    static uint8_t x = 0, y = 0;
     for(int i = 0; str[i];i++){
-        VideoMemory[i] = (VideoMemory[i] & 0xff00) | str[i];
+
+        switch (str[i])
+        {
+        case '\n':
+            y++;
+            x = 0;
+            break;
+        
+        default:
+            VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0xff00) | str[i];
+            x++;
+            break;
+        }
+
+        if(x >= 80){
+            x = 0;
+            y++;
+        }
+
+        if(y >= 25){
+            for(y = 0; y < 25; y++){
+                for(x = 0; x < 80; x++){
+                    VideoMemory[i] = (VideoMemory[i] & 0xff00) | ' ';
+                }
+            }
+            x = 0; y = 0;
+        }
+    
     }
 }
 
@@ -30,6 +59,9 @@ extern "C" void callConstructors(){
 
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t magicnumber){
     printf((char*)"hello world!");
+    printf((char*)'\n');
+    printf((char*)"hello world!");
+
     while(1);
 }
 
