e12ada7b9c5020ef958c7e89373e7b8191650fa8
Puxuan Wang <wangpu@in.tum.de>
2021-03-26 12:55:13 +0100

    new updates on interrupts

--- interrupts.cpp
+++ interrupts.cpp
@@ -3,6 +3,10 @@
 void printf(const char*);
 
 uint32_t InterruptManager::handleInterrupt(uint8_t interruptNumber, uint32_t esp){
-    return 0;
+    printf("interrupt");
+    return esp;
 }
 
+void InterruptManager::HandleInterruptRequest0x00(){}
+
+void InterruptManager::HandleException0x00(){}
